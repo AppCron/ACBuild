cbd2d6c3c758b106fd160117f79bc159393266ae
Florian Rieger <florian.rieger@appcron.com>
2016-07-20 13:59:44 +0200

    Added export step.

--- acbuild-release.sh
+++ acbuild-release.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 ACScheme="MyScheme"
+ACExportPlistPath="acbuild-release.plist"
 
 echo "### ACBuild: Starting Build..."
 echo
 
-#   Build the app with release configuration.
 echo "### ACBuild: Running archive build ..."
 echo
 xcodebuild -scheme "$ACScheme" archive -archivePath "build/ACBuild/$ACScheme.xcarchive"
+echo
+
+echo "### ACBuild: Exporting archive to IPA ..."
+echo "### Export options PLIST path: $ACExportPlistPath"
+echo
+xcodebuild -exportArchive -archivePath "build/ACBuild/$ACScheme.xcarchive" -exportOptionsPlist "$ACExportPlistPath" -exportPath "build/ACBuild"
