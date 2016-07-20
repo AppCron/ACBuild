31363df066289398b34dfb8f67fb8e527697ee84
Florian Rieger <florian.rieger@appcron.com>
2016-07-20 13:21:26 +0200

    Added build step.

--- acbuild-release.sh
+++ acbuild-release.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+ACScheme="MyScheme"
+
 echo "### ACBuild: Starting Build..."
+echo
+
+#   Build the app with release configuration.
+echo "### ACBuild: Running archive build ..."
+echo
+xcodebuild -scheme "$ACScheme" archive -archivePath "build/ACBuild/$ACScheme.xcarchive"
